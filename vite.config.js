627e3e8f585c55e41d2325ec40447efb82b8f901
CodeWithSindhu <kevalsindhu90@gmail.com>
2026-01-31 15:13:07 +0530

    chg done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/travel-guide/",
+  base: "/Travel-guide/",
   plugins: [react(), tailwindcss()],
 });
