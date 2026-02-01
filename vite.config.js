db30aedcec1aa8670cc464917ebf69523989f4f9
CodeWithSindhu <kevalsindhu90@gmail.com>
2026-02-01 12:29:27 +0530

    vite config changes done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Travel-guide/",
+  base: "/travel-guide/",
   plugins: [react(), tailwindcss()],
 });
